5db78c28eb66937a9320c7ea080b9613a2ef7d83
avr87 <annavanruiten@gmail.com>
2024-02-08 12:09:37 +0000

    function to add vote

--- src/components/CommentsByArticle.jsx
+++ src/components/CommentsByArticle.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { getCommentsByArticleId } from "../api/api";
 import CommentCard from "./CommentCard";
+import AddCommentCard from "./AddCommentCard";
+import ErrorMessage from "./ErrorMessage";
 
 export default function CommentsByArticle({
   article_id,
@@ -9,28 +11,42 @@ export default function CommentsByArticle({
   comments,
   setComments,
 }) {
+  const [error, setError] = useState();
   useEffect(() => {
-    getCommentsByArticleId(article_id).then((data) => {
-      setComments(data.comments);
-      setIsLoading(false);
-    });
+    getCommentsByArticleId(article_id)
+      .then((data) => {
+        setComments(data.comments);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
   }, [article_id]);
   if (isLoading) {
     return <p>Loading comments....</p>;
   }
-  
+
+
   return (
     <>
       <section>
         <div className="container comment-container">
           <div className="row row-cols-0.5 row-spacing">
-            {comments.map((comment) => {
-              return (
-                <div className="col" key={comment.comment_id}>
-                  <CommentCard comment={comment} />
-                </div>
-              );
-            })}
+            {comments ? (
+              comments.map((comment) => {
+                return (
+                  <div className="col" key={comment.comment_id}>
+                    <CommentCard comment={comment} />
+                  </div>
+                );
+              })
+            ) : (
+              <div className="add-comment-card">
+                <ErrorMessage />
+                <AddCommentCard />
+              </div>
+            )}
           </div>
         </div>
       </section>
